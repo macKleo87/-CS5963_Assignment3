4dacb58ca1b7e294ac9abb559e30b742f445e6d4
Leo <u0986505@utah.edu>
2019-10-26 16:13:09 -0600

    Added shapes and started BloomEffect.cs

---
+++ CS5963_Assignment3/Assets/Scripts/BloomEffect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class BloomEffect : MonoBehaviour
+{
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        RenderTexture r = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
+        //Graphics.Blit(source, destination);
+        Graphics.Blit(source, r);
+        Graphics.Blit(r, destination);
+        RenderTexture.ReleaseTemporary(r);
+    }
+}
