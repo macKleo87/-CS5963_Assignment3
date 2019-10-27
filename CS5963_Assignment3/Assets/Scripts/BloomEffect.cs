5943f715a5a5fffb0833d7b170b8df6e7e32e5c7
Kelsey <kelseyfink@me.com>
2019-10-27 17:31:58 -0600

    Finished part 2

--- CS5963_Assignment3/Assets/Scripts/BloomEffect.cs
+++ CS5963_Assignment3/Assets/Scripts/BloomEffect.cs
@@ -4,13 +4,60 @@ using System;
 [ExecuteInEditMode, ImageEffectAllowedInSceneView]
 public class BloomEffect : MonoBehaviour
 {
+    [Range(1, 16)]
+    public int iterations = 1;
+    public Shader bloomShader;
+
+
+    [NonSerialized]
+    Material bloom;
 
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        RenderTexture r = RenderTexture.GetTemporary(source.width, source.height, 0, source.format);
-        //Graphics.Blit(source, destination);
-        Graphics.Blit(source, r);
-        Graphics.Blit(r, destination);
-        RenderTexture.ReleaseTemporary(r);
+        const int BoxDownPass = 0;
+        const int BoxUpPass = 1;
+
+        if (bloom == null)
+        {
+            bloom = new Material(bloomShader);
+            bloom.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        RenderTexture[] textures = new RenderTexture[16];
+        int width = source.width / 2;
+        int height = source.height / 2;
+        RenderTextureFormat format = source.format;
+
+        RenderTexture currentDestination = textures[0] = RenderTexture.GetTemporary(width, height, 0, format);
+
+        Graphics.Blit(source, currentDestination, bloom, BoxDownPass);
+        RenderTexture currentSource = currentDestination;
+
+        int i = 1;
+        for (; i < iterations; i++)
+        {
+            width /= 2;
+            height /= 2;
+            if (height < 2)
+            {
+                break;
+            }
+            currentDestination = textures[i] = RenderTexture.GetTemporary(width, height, 0, format);
+            Graphics.Blit(currentSource, currentDestination, bloom, BoxDownPass);
+            //RenderTexture.ReleaseTemporary(currentSource);
+            currentSource = currentDestination;
+        }
+
+        for (i -= 2; i >= 0; i--)
+        {
+            currentDestination = textures[i];
+            textures[i] = null;
+            Graphics.Blit(currentSource, currentDestination, bloom, BoxUpPass);
+            RenderTexture.ReleaseTemporary(currentSource);
+            currentSource = currentDestination;
+        }
+
+        Graphics.Blit(currentSource, destination, bloom, BoxUpPass);
+        RenderTexture.ReleaseTemporary(currentSource);
     }
 }
